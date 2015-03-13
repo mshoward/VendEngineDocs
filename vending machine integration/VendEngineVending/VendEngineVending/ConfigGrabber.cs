b047074cda811724e786f28821f9dbe38c199d3f
mshoward <mshoward42@gmail.com>
2015-03-13 16:48:26 -0500

    Continuing of separation of concerns
    Trying to make installation an easymode process, maintaining an easymode
    process, and understanding the program easier.

---
+++ vending machine integration/VendEngineVending/VendEngineVending/ConfigGrabber.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace VendEngineVending
+{
+	public class ConfigGrabber
+	{
+		/**
+		 * Configuration reader upon load.
+		 * Syntax for configuration is planned to be
+		 * someVar = value;
+		 * for simplicity.
+		 */
+		public ConfigGrabber ()
+		{
+		}
+	}
+}
+
---
+++ vending machine integration/VendEngineVending/VendEngineVending/ConnectionHeartbeat.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace VendEngineVending
+{
+	public class ConnectionHeartbeat
+	{
+		/**
+		 * Class will run its thing in a separate thread and
+		 * communicate with the VendingStateMachine.
+		 * 
+		 * 
+		 */
+		public ConnectionHeartbeat ()
+		{
+		}
+	}
+}
+
