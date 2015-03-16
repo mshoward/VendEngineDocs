b95c679d0988b49b9d9a0c84fc52f22c838753d5
mshoward <mshoward42@gmail.com>
2015-03-16 12:59:34 -0500

    logic for connection dropping handling

--- vending machine integration/VendEngineVending/VendEngineVending/ConnectionHeartbeat.cs
+++ vending machine integration/VendEngineVending/VendEngineVending/ConnectionHeartbeat.cs
@@ -8,17 +8,83 @@ namespace VendEngineVending
 		 * Class will run its thing in a separate thread and
 		 * communicate with the VendingStateMachine.
 		 * 
-		 * 
+		 * runs a timer to test / keep alive connection
+		 * connection failure results in another timer starting
+		 * after second timer's elapsed time passes, event is triggered which
+		 * results in the waiting mode trigger
 		 */
 		
 		public static System.Collections.Generic.List<string> addresses;
-		public static int max_timeout;
+		public static System.Timers.Timer timeout_timer;
+		public static System.Timers.Timer timeout_tester;
 		
+		
+		public static int max_sec_timeout;
+		public static int min_sec_timeout_test;
+		
+		public static bool is_connected;
+		
+		public bool timeout_started;
 		
 		public ConnectionHeartbeat ()
 		{
+			timeout_started = false;
+			max_sec_timeout = 15;
+			min_sec_timeout_test = 3;
+			
+			timeout_tester = new System.Timers.Timer();
+			timeout_tester.Interval = min_sec_timeout_test * 1000;
+			timeout_tester.Enabled = true;
+			timeout_tester.Elapsed += new System.Timers.ElapsedEventHandler(test_timeout);
+			
+			timeout_timer = new System.Timers.Timer();
+			timeout_timer.Interval = max_sec_timeout * 1000;
+			timeout_timer.Elapsed += new System.Timers.ElapsedEventHandler(test_loss_time);
+			timeout_timer.Enabled = false;
 			
 		}
-	}
+		
+		private static bool test_connection()
+		{
+			//TODO
+		}
+		
+		public static void monitor_connection()
+		{
+			timeout_tester.Stop();
+			timeout_tester.Start();
+		}
+		
+		private static void test_timeout(object source, System.Timers.ElapsedEventArgs e)
+		{
+			if(!test_connection())
+			{
+				is_connected = false;
+				if (!timeout_started)
+				{
+					timeout_timer.Start();
+					timeout_started = true;
+				}
+			}
+			else
+			{
+				is_connected = true;
+				if(timeout_started)
+				{
+					timeout_timer.Stop();
+					timeout_started = false;
+				}
+			}
+		}
+		private static void test_loss_time(object source, System.Timers.ElapsedEventArgs e)
+		{
+			//TODO
+			//close up shop, wait for connection
+		}
+		
+		
+		
+		
+	}//end ConnectionHeartbeat
 }
 
