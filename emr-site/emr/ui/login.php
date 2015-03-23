2fd751b6ec21ea9c029efe4d67f09b01de398752
mshoward <mshoward42@gmail.com>
2015-03-23 10:47:08 -0500

    testing

---
+++ emr-site/emr/ui/login.php
@@ -0,0 +1,14 @@
+<?php
+	$login_page = '<!DOCTYPE html>
+	<div class="jumbotron">
+			<div class="container">
+				<h1>This... <small>is the login page!</small></h1>
+				<p>Ideally, this is where you'.'d log in...</p>
+			</div>
+		</div>';
+
+
+
+
+	echo $login_page;
+?>
